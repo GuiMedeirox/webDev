aba6fd39b2f408dd876cd638a6d75c60dc1b9ed7
Guilherme Medeiros <omaewa9111@gmail.com>
2022-06-11 15:46:52 -0300

    finalização script.js
    passando o this como parâmetro, é possibilitado o acesso ao backgroundColor da variavel na checkColor()

--- RGB/script.js
+++ RGB/script.js
@@ -1,22 +1,23 @@
+let corEscolhida; 
 function generateColors(){
     let div = document.getElementsByClassName("color");
     let visor = document.getElementById("painel"); 
     let r; 
     let g; 
     let b; 
-
     for(let i = 0; i < div.length; i++){
         r = Math.random() * 256; 
         g = Math.random() * 256; 
         b = Math.random() * 256; 
         div[i].style.backgroundColor = `rgb(${r},${g},${b})`; 
     }
-
-    let corEscolhida = Math.floor(Math.random() * 6);
+    corEscolhida = Math.floor(Math.random() * 6);
     visor.innerHTML = div[corEscolhida].style.backgroundColor ; 
-
 }
 
-function checkColor(){
+console.log(corEscolhida);
 
+
+function checkColor(aux){
+    
 }
