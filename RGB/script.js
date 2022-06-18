bd2931629bb18c8ece193627462b98835f147a1f
Guilherme Medeiros <omaewa9111@gmail.com>
2022-06-18 10:23:17 -0300

    finalização RGB Game

--- RGB/script.js
+++ RGB/script.js
@@ -1,23 +1,25 @@
-let corEscolhida; 
-function generateColors(){
-    let div = document.getElementsByClassName("color");
-    let visor = document.getElementById("painel"); 
-    let r; 
-    let g; 
-    let b; 
-    for(let i = 0; i < div.length; i++){
-        r = Math.random() * 256; 
-        g = Math.random() * 256; 
-        b = Math.random() * 256; 
-        div[i].style.backgroundColor = `rgb(${r},${g},${b})`; 
-    }
-    corEscolhida = Math.floor(Math.random() * 6);
-    visor.innerHTML = div[corEscolhida].style.backgroundColor ; 
+let corEscolhida;
+function generateColors() {
+  let div = document.getElementsByClassName("color");
+  let visor = document.getElementById("painel");
+  let r;
+  let g;
+  let b;
+  for (let i = 0; i < div.length; i++) {
+    r = Math.random() * 256;
+    g = Math.random() * 256;
+    b = Math.random() * 256;
+    div[i].style.backgroundColor = `rgb(${r},${g},${b})`;
+  }
+  corEscolhida = Math.floor(Math.random() * 6);
+  visor.innerHTML = div[corEscolhida].style.backgroundColor;
 }
 
-console.log(corEscolhida);
-
-
-function checkColor(aux){
-    
+function checkColor(aux) {
+  let corCerta = document.getElementById("painel");
+  if (corCerta.textContent === aux.style.backgroundColor) {
+    alert(true);
+  } else {
+    alert(false);
+  }
 }
